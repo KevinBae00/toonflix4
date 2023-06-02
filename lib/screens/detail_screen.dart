b8437bea55edb7e58291da7a9baddaac328ffc3b
username <useremail>
2023-06-02 17:27:50 +0900

    06-02

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -46,13 +46,7 @@ class DetailScreen extends StatelessWidget {
                         color: Colors.black.withOpacity(0.5)),
                   ],
                 ),
-                child: Image.network(
-                  thumb,
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                  },
-                ),
+                child: Image.network(thumb),
               ),
             ],
           )
